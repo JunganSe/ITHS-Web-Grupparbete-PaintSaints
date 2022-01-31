bf96d4d1d1953ad00f694c22405bdabafd4ef05c
Dennis Stegnell <dennis@stegnell.com>
2022-01-31 14:41:42 +0100

    addat menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function dropDownMenu() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("Menu").classList.toggle("show");
   }
 
   window.onclick = function(event) {
@@ -14,3 +14,22 @@ function dropDownMenu() {
       }
     }
   }
+
+  // function expandMenu() {
+  //   document.getElementById("Artists").classList.toggle("show");
+  // }
+
+  // window.onclick = function(event) {
+  //   if (!event.target.matches('.dropbtn2')) {
+  //     var dropdowns = document.getElementsByClassName("artistMenu");
+  //     var i;
+  //     for (i = 0; i < dropdowns.length; i++) {
+  //       var openDropdown = dropdowns[i];
+  //       if (openDropdown.classList.contains('show')) {
+  //         openDropdown.classList.remove('show');
+  //       }
+  //     }
+  //   }
+  // }
+
+  
