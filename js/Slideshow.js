c8cd63826dc15cb9d737be282d77d581b6d4a534
Johan Ljungberg <jlj.9@hotmail.com>
2022-02-07 15:52:48 +0100

    kodstädning

--- js/Slideshow.js
+++ js/Slideshow.js
@@ -1,37 +1,37 @@
 var playing = true;
-var pauseButton = document.getElementById('pause');
-var myIndex = 0;
+var pauseButton = document.getElementById('slideshow-pause-button');
+var index = 0;
 
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("slides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    var pictures = document.getElementsByClassName("slideshow-slides");
+    for (i = 0; i < pictures.length; i++) {
+        pictures[i].style.display = "none";
     }
-    myIndex++;
-    if (myIndex > x.length) {
-        myIndex = 1
+    index++;
+    if (index > pictures.length) {
+        index = 1
     }
-    x[myIndex - 1].style.display = "block";
+    pictures[index - 1].style.display = "block";
 }
 
-function pauseSlideshow(){
+function pauseSlideshow() {
     pauseButton.innerHTML = 'Play';
     playing = false;
     clearInterval(slideInterval);
 }
 
-function playSlideshow(){
+function playSlideshow() {
     pauseButton.innerHTML = 'Pause';
     playing = true;
     slideInterval = setInterval(carousel, 4000);
 }
 
-pauseButton.onclick = function(){
-    if(playing){
+pauseButton.onclick = function() {
+    if (playing) {
         pauseSlideshow();
     }
-    else{
+    else {
         playSlideshow();
     }
 };
